7bf59a7ba9b6054cbf0310fcadccf4c628128706
alexdx3 <sgoleksandr@gmail.com>
2017-10-21 23:58:16 +0200

    Create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myRougeLike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
